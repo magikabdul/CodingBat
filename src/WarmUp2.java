281cbde0ab54183bd1b467227d41217ce2c81038
Krzysztof Cholewa <krzysztof.cholewa@gmail.com>
2017-11-06 00:25:27 +0100

    first three methods

--- src/WarmUp2.java
+++ src/WarmUp2.java
@@ -1,4 +1,48 @@
-package PACKAGE_NAME;
-
 public class WarmUp2 {
+
+    public String stringTimes(String str, int n) {
+        StringBuilder s = new StringBuilder();
+
+        for (int i = 0; i < n; i++) {
+            s.append(str);
+        }
+
+        return s.toString();
+    }
+
+    public boolean noTriples(int[] nums) {
+        for (int i = 0; i < nums.length - 2; i++) {
+            if(nums[i] == nums[i+1] && nums[i+1] == nums[i+2]) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public int arrayCount9(int[] nums) {
+        int count = 0;
+
+        for(int i = 0; i < nums.length; i ++) {
+            if (nums[i] == 9) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+
+    
+}
+
+class App {
+
+    public static void main(String[] args) {
+        WarmUp2 w = new WarmUp2();
+
+        //System.out.println(w.());
+    }
+
+
+
 }
