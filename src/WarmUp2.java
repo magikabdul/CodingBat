ad40111858553b47533d7aaca349bb310ec29a97
Krzysztof Cholewa <krzysztof.cholewa@gmail.com>
2017-11-07 22:11:52 +0100

    few methods

--- src/WarmUp2.java
+++ src/WarmUp2.java
@@ -48,6 +48,77 @@ public class WarmUp2 {
             return false;
         }
     }
+
+    public boolean arrayFront9(int[] nums) {
+
+        for (int i = 0; i < nums.length; i++) {
+            if(i < 4 && nums[i] == 9) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public String stringSplosion(String str) {
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < str.length(); i++) {
+            sb.append(str.substring(0, i));
+        }
+
+        sb.append(str);
+
+        return sb.toString();
+    }
+
+    public String frontTimes(String str, int n) {
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < n; i++) {
+            if (str.length() < 3) {
+                sb.append(str);
+            }   else {
+                sb.append(str.substring(0, 3));
+            }
+        }
+
+        return sb.toString();
+    }
+
+    public int array667(int[] nums) {
+
+        int count = 0;
+
+        if(nums.length < 2) {
+            return 0;
+        }   else {
+
+            for (int i = 0; i < nums.length - 1; i++) {
+                if ((nums[i] == 6 && nums[i] == nums[i + 1]) || (nums[i] == 6 && nums[i] == nums[i + 1] - 1)) {
+                    count++;
+                }
+            }
+            return count;
+        }
+    }
+
+    public String stringX(String str) {
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < str.length(); i++) {
+            if (i == 0 || i == str.length() - 1) {
+                sb.append(str.charAt(i));
+            }
+            else if (!str.substring(i, i+1).equals("x")) {
+                sb.append(str.charAt(i));
+            }
+        }
+
+        return sb.toString();
+    }
 }
 
 class App {
@@ -55,7 +126,7 @@ class App {
     public static void main(String[] args) {
         WarmUp2 w = new WarmUp2();
 
-        System.out.println(w.has271(new int[]{2, 7, 4}));
+        System.out.println(w.stringX("xxdupaxx"));
     }
 
 
