a61681d21587da7442083a575378635cef920d04
Krzysztof Cholewa <krzysztof.cholewa@gmail.com>
2017-11-08 19:15:33 +0100

    three methods left

--- src/WarmUp2.java
+++ src/WarmUp2.java
@@ -119,6 +119,74 @@ public class WarmUp2 {
 
         return sb.toString();
     }
+
+    public int countXX(String str) {
+
+        int count = 0;
+
+        for (int i = 0; i < str.length() - 1; i++) {
+            if((str.substring(i,i+1).equals(str.substring(i+1,i+2))) && str.substring(i, i + 1).equals("x")){
+                count++;
+            }
+        }
+
+        return count;
+    }
+
+    public String altPairs(String str) {
+
+        int take = 0;
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < str.length(); i++) {
+            if(take == 0 || take == 1) {
+                sb.append(str.charAt(i));
+            }
+
+            take++;
+            if(take > 3) { take = 0; }
+        }
+
+        return sb.toString();
+    }
+
+    public int stringMatch(String a, String b) {
+
+        int count = 0;
+        int loopRange = 0;
+
+        if (a.length() <= b.length()) { loopRange = a.length(); }
+        else { loopRange = b.length(); }
+
+        for (int i = 0; i < loopRange - 1; i++) {
+            if (a.substring(i, i + 2).equals(b.substring(i, i + 2))) { count++; }
+        }
+
+        return count;
+    }
+
+    public int last2(String str) {
+        int count = 0;
+
+        for (int i = 0; i < str.length() - 2; i++) {
+            if(str.substring(str.length() - 2, str.length()).equals(str.substring(i, i+2))) { count++; }
+        }
+
+        return count;
+    }
+
+    public boolean doubleX(String str) {
+
+        for (int i = 0; i < str.length() - 1; i++) {
+            if (str.substring(i, i+1).equals("x")) {
+                if (str.substring(i+1, i+2).equals("x")) { return true; }
+                else { return false; }
+            }
+        }
+
+        return false;
+    }
 }
 
 class App {
@@ -126,7 +194,7 @@ class App {
     public static void main(String[] args) {
         WarmUp2 w = new WarmUp2();
 
-        System.out.println(w.stringX("xxdupaxx"));
+        System.out.println(w.doubleX("axaaxx"));
     }
 
 
