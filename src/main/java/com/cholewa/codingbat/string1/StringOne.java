b00b565f63024f35b73d03d275c5a3e0556b6233
Krzysztof Cholewa <krzysztof.cholewa@gmail.com>
2017-11-27 17:32:56 +0100

    startWord - not working

--- src/main/java/com/cholewa/codingbat/string1/StringOne.java
+++ src/main/java/com/cholewa/codingbat/string1/StringOne.java
@@ -4,6 +4,65 @@ public class StringOne {
 
     public String helloName(String name) {
 
-        return null;
+        return "Hello " + name + "!";
+    }
+
+    /*
+    *   Given a string, return true if "bad" appears starting at index 0 or 1 in the string, such as with "badxxx" or "xbadxx" but not "xxbadxx".
+    *   The string may be any length, including 0. Note: use .equals() to compare 2 strings.
+
+        hasBad("badxx") → true
+        hasBad("xbadxx") → true
+        hasBad("xxbadxx") → false
+    * */
+
+    public boolean hasBad(String str) {
+        final String CODE = "bad";
+
+        if (str.length() < 3) {
+            return false;
+        } else if (str.length() == 3) {
+            if (str.equals(CODE)) {
+                return true;
+            } else {
+                return false;
+            }
+        } else if (str.length() == 4) {
+            if (str.substring(0, 3).equals(CODE) || str.substring(1).equals(CODE)) {
+                return true;
+            } else {
+                return false;
+            }
+        } else {
+            if (str.substring(0, 3).equals(CODE) || str.substring(1, 4).equals(CODE)) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+    }
+
+    /*
+    *   Given a string and a second "word" string, we'll say that the word matches the string if it appears at the front of the string,
+    *   except its first char does not need to match exactly. On a match, return the front of the string, or otherwise return the empty string.
+    *   So, so with the string "hippo" the word "hi" returns "hi" and "xip" returns "hip". The word will be at least length 1.
+    *
+    *   startWord("hippo", "hi") → "hi"
+    *   startWord("hippo", "xip") → "hip"
+    *   startWord("hippo", "i") → "h"
+    *
+    * */
+
+    public String startWord(String str, String word) {
+
+        if (str.length() >= word.length()) {
+            if (str.substring(1, word.length() + 1).equals(word.substring(1))) {
+                return str.substring(0, 1 + word.length());
+            } else {
+                return "";
+            }
+        } else {
+            return "";
+        }
     }
 }
--- src/test/java/com/cholewa/codingbat/string1/StringOneTestSuite.java
+++ src/test/java/com/cholewa/codingbat/string1/StringOneTestSuite.java
@@ -1,29 +1,66 @@
 package com.cholewa.codingbat.string1;
 
 import org.junit.Assert;
-import org.junit.Before;
 import org.junit.Test;
 
 public class StringOneTestSuite {
-    private StringOne stringOne;
-
-    @Before
-    public void before() {
-        stringOne = new StringOne();
-    }
 
     @Test
     public void testHelloName() {
         //Given
+        StringOne stringOne = new StringOne();
 
         //When
         String result1 = stringOne.helloName("Bob");
         String result2 = stringOne.helloName("Alice");
         String result3 = stringOne.helloName("X");
+        String result4 = stringOne.helloName("");
 
         //Then
         Assert.assertEquals("Hello Bob!", result1);
         Assert.assertEquals("Hello Alice!", result2);
-        Assert.assertEquals("X!", result3);
+        Assert.assertEquals("Hello X!", result3);
+        Assert.assertEquals("Hello !", result4);
+    }
+
+    @Test
+    public void testHasBad() {
+        //Given
+        StringOne stringOne = new StringOne();
+
+        //When
+        boolean result1 = stringOne.hasBad("badxx");
+        boolean result2 = stringOne.hasBad("xbadxx");
+        boolean result3 = stringOne.hasBad("xxbadxx");
+        boolean result4 = stringOne.hasBad("");
+        boolean result5 = stringOne.hasBad("xba");
+        boolean result6 = stringOne.hasBad("bad");
+
+        //Then
+        Assert.assertTrue(result1);
+        Assert.assertTrue(result2);
+        Assert.assertFalse(result3);
+        Assert.assertFalse(result4);
+        Assert.assertFalse(result5);
+        Assert.assertTrue(result6);
+    }
+
+    @Test
+    public void testStartWord() {
+        //Given
+        StringOne stringOne = new StringOne();
+
+        //When
+        String result1 = stringOne.startWord("hippo", "hi");
+        String result2 = stringOne.startWord("hippo", "xip");
+        String result3 = stringOne.startWord("hippo", "i");
+        String result4 = stringOne.startWord("", "a");
+
+
+        //Then
+        Assert.assertEquals("hi", result1);
+        Assert.assertEquals("hip", result2);
+        Assert.assertEquals("h", result3);
+        Assert.assertEquals("", result4);
     }
 }
