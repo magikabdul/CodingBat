cfe4feb4197536b0a6d4c4dff32108c7d17e1fd2
Krzysztof Cholewa <krzysztof.cholewa@gmail.com>
2017-12-21 14:09:21 +0100

    String1 - finished

--- src/main/java/coding/bat/string/one/StringOne.java
+++ src/main/java/coding/bat/string/one/StringOne.java
@@ -86,13 +86,224 @@ public class StringOne {
         return stringBuilder.toString();
     }
 
+    public String theEnd(String str, boolean front) {
+        if (front == true) {
+            return str.substring(0, 1);
+        } else {
+            return str.substring(str.length() - 1);
+        }
+    }
 
+    public String withouEnd2(String str) {
+        if (str.length() < 3) {
+            return "";
+        } else {
+            return str.substring(1, str.length() - 1);
+        }
+    }
 
-//    theEnd	 withouEnd2	 middleTwo
-//    endsLy	 nTwice	 twoChar
-//    middleThree	 hasBad	 atFirst
-//    lastChars	 conCat	 lastTwo
-//    seeColor	 frontAgain	 minCat
-//    extraFront	 without2	 deFront
-//    startWord	 withoutX	 withoutX2
+    public String middleTwo(String str) {
+        return str.substring(str.length() / 2 - 1, str.length() / 2 + 1);
+    }
+
+    public boolean endsLy(String str) {
+        return (str.endsWith("ly"));
+    }
+
+    public String nTwice(String str, int n) {
+        return str.substring(0, n) + str.substring(str.length() - n, str.length());
+    }
+
+    public String twoChar(String str, int index) {
+        if (str.length() - 2 < index || index < 0) {
+            return str.substring(0,2);
+        } else {
+            return str.substring(index, index + 2);
+        }
+    }
+
+    public String middleThree(String str) {
+        return str.substring(str.length() / 2 - 1, str.length() / 2 + 2);
+    }
+
+    public boolean hasBad(String str) {
+        final String CODE = "bad";
+
+        if (str.length() < 3) {
+            return false;
+        } else if (str.length() == 3) {
+            return (str.equals(CODE));
+        } else if (str.length() == 4) {
+            return (str.substring(0, 3).equals(CODE) || str.substring(1).equals(CODE)) ;
+        } else {
+            return (str.substring(0, 3).equals(CODE) || str.substring(1, 4).equals(CODE));
+        }
+    }
+
+    public String atFirst(String str) {
+        if (str.length() < 2){
+            if (str.length() == 1) {
+                return str + "@";
+            } else {
+                return "@@";
+            }
+        } else {
+            return str.substring(0, 2);
+        }
+    }
+
+    public String lastChars(String a, String b) {
+        if (a.length() > 0 && b.length() > 0) {
+            return a.substring(0, 1) + b.substring(b.length() - 1);
+        } else {
+            if (a.length() == 0 && b.length() == 0) {
+                return "@@";
+            } else if (a.length() == 0) {
+                return "@" + b.substring(b.length() - 1);
+            } else {
+                return a.substring(0, 1) + "@";
+            }
+        }
+    }
+
+    public String conCat(String a, String b) {
+        if (a.length() > 0 && b.length() > 0) {
+            if (a.substring(a.length() - 1).equals(b.substring(0, 1))) {
+                return a + b.substring(1);
+            } else {
+                return a + b;
+            }
+        } else if (a.length() == 0 && b.length() == 0) {
+            return "";
+        } else if (a.length() == 0) {
+            return b;
+        } else {
+            return a;
+        }
+    }
+
+    public String lastTwo(String str) {
+        if (str.length() > 1){
+            return str.substring(0, str.length() - 2) + str.substring(str.length() - 1, str.length()) +
+                    str.substring(str.length() - 2, str.length() - 1);
+        } else {
+            return str;
+        }
+    }
+
+    public String seeColor(String str) {
+        if (str.startsWith("red")) {
+            return "red";
+        } else if (str.startsWith("blue")) {
+            return "blue";
+        } else {
+            return "";
+        }
+    }
+
+    public boolean frontAgain(String str) {
+        return (str.length() > 1 && str.substring(0, 2).equals(str.substring(str.length() - 2, str.length())));
+    }
+
+    public String minCat(String a, String b) {
+        if (a.length() == b.length()){
+            return a + b;
+        } else if (a.length() > b.length()){
+            return a.substring(a.length() - b.length(), a.length()) + b;
+        } else {
+            return a + b.substring(b.length() - a.length(), b.length());
+        }
+    }
+
+    public String extraFront(String str) {
+        if (str.length() < 2){
+            return str + str + str;
+        } else {
+            return str.substring(0, 2) + str.substring(0, 2) + str.substring(0, 2);
+        }
+    }
+
+    public String without2(String str) {
+        if (str.length() == 1) {
+            return str;
+        } else if (str.length() < 3) {
+            return "";
+        } else if (str.substring(str.length() - 2).equals(str.substring(0, 2))) {
+            return str.substring(2);
+        } else {
+            return str;
+        }
+    }
+
+    public String deFront(String str) {
+        StringBuilder sb = new StringBuilder();
+
+        if (str.length() >= 1) {
+            if (str.charAt(0) == 'a') {
+                sb.append(str.substring(0, 1));
+            }
+        }
+        if (str.length() >= 2) {
+            if (str.charAt(1) == 'b') {
+                sb.append(str.substring(1, 2));
+            }
+        }
+        if (str.length() > 2) {
+
+            sb.append(str.substring(2));
+        }
+
+        return sb.toString();
+    }
+
+    public String startWord(String str, String word) {
+        int comparatorLength = word.length();
+
+        if (str.length() < word.length()) {
+            return "";
+        } else if (str.length() == word.length()) {
+            if (str.substring(1).equals(word.substring(1))) {
+                return str.substring(0);
+            }
+        } else {
+            if (str.substring(1, comparatorLength).equals(word.substring(1))) {
+                return str.substring(0, comparatorLength);
+            }
+        }
+
+        return "";
+    }
+
+    public String withoutX(String str) {
+        if (str.startsWith("x") && str.endsWith("x")) {
+            if (str.length() > 1) {
+                return str.substring(1, str.length() - 1);
+            } else {
+                return "";
+            }
+        } else if (str.startsWith("x")) {
+            if (str.length() > 0) {
+                return str.substring(1);
+            } else {
+                return "";
+            }
+        } else if (str.endsWith("x")) {
+            return str.substring(0, str.length() - 1);
+        } else {
+            return str;
+        }
+    }
+
+    public String withoutX2(String str) {
+        StringBuilder stringBuilder = new StringBuilder();
+
+        for (int i = 0; i < str.length(); i++){
+            if (i < 2 && !str.substring(i, i+1).equals("x")){
+                stringBuilder.append(str.substring(i, i + 1));
+            }   else if (i >= 2){
+                stringBuilder.append(str.substring(i, i + 1));
+            }
+        }
+        return stringBuilder.toString();
+    }
 }
--- src/test/java/coding/bat/string/one/StringOneTestSuite.java
+++ src/test/java/coding/bat/string/one/StringOneTestSuite.java
@@ -167,4 +167,283 @@ public class StringOneTestSuite {
         Assert.assertEquals("vaja", result2);
         Assert.assertEquals("Hi", result3);
     }
+
+    @Test
+    public void testTheEnd() {
+        //When
+        String result1 = one.theEnd("Hello", true);
+        String result2 = one.theEnd("Hello", false);
+        String result3 = one.theEnd("oh", true);
+
+        //Then
+        Assert.assertEquals("H", result1);
+        Assert.assertEquals("o", result2);
+        Assert.assertEquals("o", result3);
+    }
+
+    @Test
+    public void testWithouEnd2() {
+        //When
+        String result1 = one.withouEnd2("Hello");
+        String result2 = one.withouEnd2("abc");
+        String result3 = one.withouEnd2("ab");
+
+        //Then
+        Assert.assertEquals("ell", result1);
+        Assert.assertEquals("b", result2);
+        Assert.assertEquals("", result3);
+    }
+
+    @Test
+    public void testMiddleTwo() {
+        //When
+        String result1 = one.middleTwo("string");
+        String result2 = one.middleTwo("code");
+        String result3 = one.middleTwo("Practice");
+
+        //Then
+        Assert.assertEquals("ri", result1);
+        Assert.assertEquals("od", result2);
+        Assert.assertEquals("ct", result3);
+    }
+
+    @Test
+    public void testEndLy() {
+        //When
+        boolean result1 = one.endsLy("oddly");
+        boolean result2 = one.endsLy("y");
+        boolean result3 = one.endsLy("oddy");
+
+        //Then
+        Assert.assertTrue(result1);
+        Assert.assertFalse(result2);
+        Assert.assertFalse(result3);
+    }
+
+    @Test
+    public void testNTwice() {
+        //When
+        String result1 = one.nTwice("Hello",2);
+        String result2 = one.nTwice("Chocolate",3);
+        String result3 = one.nTwice("Chocolate",1);
+
+        //Then
+        Assert.assertEquals("Helo", result1);
+        Assert.assertEquals("Choate", result2);
+        Assert.assertEquals("Ce", result3);
+    }
+
+    @Test
+    public void testTwoChar() {
+        //When
+        String result1 = one.twoChar("java",0);
+        String result2 = one.twoChar("java",2);
+        String result3 = one.twoChar("java",3);
+        String result4 = one.twoChar("java",-1);
+
+        //Then
+        Assert.assertEquals("ja", result1);
+        Assert.assertEquals("va", result2);
+        Assert.assertEquals("ja", result3);
+        Assert.assertEquals("ja", result4);
+    }
+
+    @Test
+    public void testMiddleThree() {
+        //When
+        String result1 = one.middleThree("Candy");
+        String result2 = one.middleThree("and");
+        String result3 = one.middleThree("solving");
+
+        //Then
+        Assert.assertEquals("and", result1);
+        Assert.assertEquals("and", result2);
+        Assert.assertEquals("lvi", result3);
+    }
+
+    @Test
+    public void testHasBad() {
+        //When
+        boolean result1 = one.hasBad("badxx");
+        boolean result2 = one.hasBad("xbadxx");
+        boolean result3 = one.hasBad("xxbadxx");
+
+        //Then
+        Assert.assertTrue(result1);
+        Assert.assertTrue(result2);
+        Assert.assertFalse(result3);
+    }
+
+    @Test
+    public void testAtFirst() {
+        //When
+        String result1 = one.atFirst("hello");
+        String result2 = one.atFirst("hi");
+        String result3 = one.atFirst("h");
+
+        //Then
+        Assert.assertEquals("he", result1);
+        Assert.assertEquals("hi", result2);
+        Assert.assertEquals("h@", result3);
+    }
+
+    @Test
+    public void testLastChars() {
+        //When
+        String result1 = one.lastChars("last", "chars");
+        String result2 = one.lastChars("yo","java");
+        String result3 = one.lastChars("hi","");
+
+        //Then
+        Assert.assertEquals("ls", result1);
+        Assert.assertEquals("ya", result2);
+        Assert.assertEquals("h@", result3);
+    }
+
+    @Test
+    public void testConCat() {
+        //When
+        String result1 = one.conCat("abc", "cat");
+        String result2 = one.conCat("dog","cat");
+        String result3 = one.conCat("abc","");
+
+        //Then
+        Assert.assertEquals("abcat", result1);
+        Assert.assertEquals("dogcat", result2);
+        Assert.assertEquals("abc", result3);
+    }
+
+    @Test
+    public void testLastTwo() {
+        //When
+        String result1 = one.lastTwo("coding");
+        String result2 = one.lastTwo("cat");
+        String result3 = one.lastTwo("ab");
+
+        //Then
+        Assert.assertEquals("codign", result1);
+        Assert.assertEquals("cta", result2);
+        Assert.assertEquals("ba", result3);
+    }
+
+    @Test
+    public void testSeeColor() {
+        //When
+        String result1 = one.seeColor("redxx");
+        String result2 = one.seeColor("xxred");
+        String result3 = one.seeColor("blueTimes");
+
+        //Then
+        Assert.assertEquals("red", result1);
+        Assert.assertEquals("", result2);
+        Assert.assertEquals("blue", result3);
+    }
+
+    @Test
+    public void testFrontAgain() {
+        //When
+        boolean result1 = one.frontAgain("edited");
+        boolean result2 = one.frontAgain("edit");
+        boolean result3 = one.frontAgain("ed");
+
+        //Then
+        Assert.assertTrue(result1);
+        Assert.assertFalse(result2);
+        Assert.assertTrue(result3);
+    }
+
+    @Test
+    public void testMinCat() {
+        //When
+        String result1 = one.minCat("Hello", "Hi");
+        String result2 = one.minCat("Hello", "java");
+        String result3 = one.minCat("java", "Hello");
+
+        //Then
+        Assert.assertEquals("loHi", result1);
+        Assert.assertEquals("ellojava", result2);
+        Assert.assertEquals("javaello", result3);
+    }
+
+    @Test
+    public void testExtraFront() {
+        //When
+        String result1 = one.extraFront("Hello");
+        String result2 = one.extraFront("ab");
+        String result3 = one.extraFront("H");
+
+        //Then
+        Assert.assertEquals("HeHeHe", result1);
+        Assert.assertEquals("ababab", result2);
+        Assert.assertEquals("HHH", result3);
+    }
+
+    @Test
+    public void testWithout2() {
+        //When
+        String result1 = one.without2("HelloHe");
+        String result2 = one.without2("HelloHi");
+        String result3 = one.without2("Hi");
+        String result4 = one.without2("x");
+
+        //Then
+        Assert.assertEquals("lloHe", result1);
+        Assert.assertEquals("HelloHi", result2);
+        Assert.assertEquals("", result3);
+        Assert.assertEquals("x", result4);
+    }
+
+    @Test
+    public void testDeFront() {
+        //When
+        String result1 = one.deFront("Hello");
+        String result2 = one.deFront("java");
+        String result3 = one.deFront("away");
+
+        //Then
+        Assert.assertEquals("llo", result1);
+        Assert.assertEquals("va", result2);
+        Assert.assertEquals("aay", result3);
+    }
+
+    @Test
+    public void testStartWord() {
+        //When
+        String result1 = one.startWord("hippo","hi");
+        String result2 = one.startWord("hippo","hip");
+        String result3 = one.startWord("hippo","h");
+
+        //Then
+        Assert.assertEquals("hi", result1);
+        Assert.assertEquals("hip", result2);
+        Assert.assertEquals("h", result3);
+    }
+
+    @Test
+    public void testWithoutX() {
+        //When
+        String result1 = one.withoutX("xHix");
+        String result2 = one.withoutX("xHi");
+        String result3 = one.withoutX("Hxix");
+        String result4 = one.withoutX("x");
+
+        //Then
+        Assert.assertEquals("Hi", result1);
+        Assert.assertEquals("Hi", result2);
+        Assert.assertEquals("Hxi", result3);
+        Assert.assertEquals("", result4);
+    }
+
+    @Test
+    public void testWithoutX2() {
+        //When
+        String result1 = one.withoutX2("xHi");
+        String result2 = one.withoutX2("Hxi");
+        String result3 = one.withoutX2("Hi");
+
+        //Then
+        Assert.assertEquals("Hi", result1);
+        Assert.assertEquals("Hi", result2);
+        Assert.assertEquals("Hi", result3);
+    }
 }
