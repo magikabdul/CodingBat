c52b98ed94047f08c70a95c8b3b64de640b1080b
Krzysztof Cholewa <krzysztof.cholewa@gmail.com>
2017-12-27 19:31:07 +0100

    recursion one done

--- src/main/java/coding/bat/recursion/one/RecursionOne.java
+++ src/main/java/coding/bat/recursion/one/RecursionOne.java
@@ -54,4 +54,342 @@ public class RecursionOne {
             return n % 10 + sumDigits(n / 10);
         }
     }
+
+    public static int count7(int n) {
+        if (n < 7) {
+            return 0;
+        } else {
+            if (n % 10 == 7) {
+                return 1 + count7(n / 10);
+            } else {
+                return count7(n / 10);
+            }
+        }
+    }
+
+    public static int count8(int n) {
+        if (n < 8) {
+            return 0;
+        } else {
+            if (n % 10 == 8 && (n / 10) % 10 == 8) {
+                return 2 + count8(n / 10);
+            } else if (n % 10 == 8) {
+                return 1 + count8(n / 10);
+            } else {
+                return count8(n / 10);
+            }
+        }
+    }
+
+    public static int powerN(int base, int n) {
+        if (n == 0) {
+            return 1;
+        } else {
+            return base * powerN(base, n - 1);
+        }
+    }
+
+    public static int countX(String str) {
+        if (str.length() == 0) {
+            return 0;
+        } else {
+            if (str.startsWith("x")) {
+                return 1 + countX(str.substring(1));
+            } else {
+                return countX(str.substring(1));
+            }
+        }
+    }
+
+    public static int countHi(String str) {
+        if (str.length() < 2) {
+            return 0;
+        } else {
+            if (str.startsWith("hi")) {
+                return 1 + countHi(str.substring(2));
+            } else {
+                return countHi(str.substring(1));
+            }
+        }
+    }
+
+    public static String changeXY(String str) {
+        if (str.length() == 0) {
+            return "";
+        } else {
+            if (str.startsWith("x")) {
+                return "y" + changeXY(str.substring(1));
+            } else {
+                return str.substring(0, 1) + changeXY(str.substring(1));
+            }
+        }
+    }
+
+    public static String changePi(String str) {
+        if (str.length() == 0) {
+            return "";
+        } else {
+            if (str.startsWith("pi")) {
+                return "3.14" + changePi(str.substring(2));
+            } else {
+                return str.substring(0, 1) + changePi(str.substring(1));
+            }
+        }
+    }
+
+    public static String noX(String str) {
+        if (str.length() == 0) {
+            return "";
+        } else {
+            if (str.startsWith("x")) {
+                return noX(str.substring(1));
+            } else {
+                return str.substring(0, 1) + noX(str.substring(1));
+            }
+        }
+    }
+
+    public static boolean array6(int[] nums, int index) {
+        if (nums.length == 0) {
+            return false;
+        }
+        if (index == nums.length - 1) {
+            return nums[index] == 6;
+        } else {
+            if (nums[index] == 6) {
+                return true;
+            } else {
+                return array6(nums, index + 1);
+            }
+        }
+    }
+
+    public static int array11(int[] nums, int index) {
+        if (nums.length == 0) {
+            return 0;
+        }
+        if (index > nums.length - 1) {
+            return 0;
+        }
+        if (index == nums.length - 1) {
+            return nums[index] == 11 ? 1 : 0;
+        } else {
+            if (nums[index] == 11) {
+                return 1 + array11(nums, index + 1);
+            } else {
+                return array11(nums, index + 1);
+            }
+        }
+    }
+
+    public static boolean array220(int[] nums, int index) {
+        if (nums.length < 2) {
+            return false;
+        }
+
+        if (index == nums.length - 2) {
+            return nums[index] * 10 == nums[index + 1];
+        } else {
+            return (nums[index] * 10 == nums[index + 1]) || array220(nums, index + 1);
+        }
+    }
+
+    public static String allStar(String str) {
+        if (str.length() < 2) {
+            return str;
+        } else {
+            return str.substring(0, 1) + "*" + allStar(str.substring(1));
+        }
+    }
+
+    public static String pairStar(String str) {
+        if (str.length() < 2) {
+            return str;
+        } else {
+            if (str.substring(0, 1).equals(str.substring(1, 2))) {
+                return str.substring(0, 1) + "*" + pairStar(str.substring(1));
+            } else {
+                return str.substring(0,1) + pairStar(str.substring(1));
+            }
+        }
+    }
+
+    public static String endX(String str) {
+        if (str.length() < 1) {
+            return "";
+        } else {
+            if (str.startsWith("x")) {
+                return endX(str.substring(1)) + "x";
+            } else {
+                return str.substring(0, 1) + endX(str.substring(1));
+            }
+        }
+    }
+
+    public static int countPairs(String str) {
+        if (str.length() < 3) {
+            return 0;
+        } else {
+            if (str.substring(0, 1).equals(str.substring(2, 3))) {
+                return 1 + countPairs(str.substring(1));
+            } else {
+                return countPairs(str.substring(1));
+            }
+        }
+    }
+
+    public static int countAbc(String str) {
+        if (str.length() < 3) {
+            return 0;
+        } else if (str.length() == 3) {
+            if (str.equals("abc") || str.equals("aba")) {
+                return 1;
+            } else {
+                return 0;
+            }
+        } else {
+            if (str.substring(0, 3).equals("abc") || str.substring(0, 3).equals("aba")) {
+                if (str.length() < 4) {
+                    return 1;
+                } else
+                    return 1 + countAbc(str.substring(1));
+            } else {
+                return countAbc(str.substring(1));
+            }
+        }
+    }
+
+    public static int count11(String str) {
+        if (str.length() < 2) {
+            return 0;
+        } else if (str.length() == 2) {
+            if (str.equals("11")) {
+                return 1;
+            } else {
+                return 0;
+            }
+        } else {
+            if (str.substring(0, 2).equals("11")) {
+                if (str.length() < 4) {
+                    return 1;
+                } else {
+                    return 1 + count11(str.substring(2));
+                }
+            } else {
+                return count11(str.substring(1));
+            }
+        }
+    }
+
+    public static String stringClean(String str) {
+        if (str.length() < 1) {
+            return "";
+        } else if (str.length() < 2) {
+            return str;
+        } else {
+            if (str.substring(0, 1).equals(str.substring(1, 2))) {
+                return stringClean(str.substring(1));
+            } else {
+                return str.substring(0, 1) + stringClean(str.substring(1));
+            }
+        }
+    }
+
+    public static int countHi2(String str) {
+        if (str.length() < 2) {
+            return 0;
+        } else if (str.length() == 2) {
+            return str.equals("hi") ? 1 : 0;
+        } else {
+            if (str.substring(0, 1).equals("x")) {
+                if (str.substring(1, 3).equals("hi")) {
+                    return str.length() < 3 ? 0 : countHi2(str.substring(3));
+                } else {
+                    return countHi2(str.substring(1));
+                }
+            } else if (str.substring(0, 2).equals("hi")) {
+                return str.length() < 2 ? 1 : 1 + countHi2(str.substring(2));
+            } else {
+                return countHi2(str.substring(1));
+            }
+        }
+    }
+
+    public static String parenBit(String str) {
+        if (str.length() < 1) {
+            return "";
+        } else {
+            if (str.contains(")")) {
+                if (str.startsWith("(") || !str.contains("(")) {
+                    return str.substring(0, 1) + parenBit(str.substring(1));
+                } else {
+                    return parenBit(str.substring(1));
+                }
+            } else {
+                return "";
+            }
+        }
+    }
+
+    public static boolean nestParen(String str) {
+        if (str.length() < 1) {
+            return true;
+        } else if (str.length() == 2) {
+            return str.contains("(") && str.contains(")");
+        }
+
+        if (str.contains("(") && str.contains(")")) {
+            return nestParen(str.substring(str.indexOf("(") + 1, str.lastIndexOf(")")));
+        } else {
+            return false;
+        }
+    }
+
+    public static int strCount(String str, String sub) {
+        if (str.length() < sub.length()) {
+            return 0;
+        } else if (str.length() == sub.length()) {
+            return str.equals(sub) ? 1 : 0;
+        } else {
+            if (str.contains(sub)) {
+                return 1 + strCount(str.substring(str.indexOf(sub) + sub.length()), sub);
+            } else {
+                return 0;
+            }
+        }
+    }
+
+    public static boolean strCopies(String str, String sub, int n) {
+        if (n == 0) {
+            return true;
+        } else if (str.length() < sub.length()) {
+            return false;
+        } else if (str.length() == sub.length()) {
+            return n == 1 && str.contains(sub);
+        } else {
+            return str.contains(sub) && strCopies(str.substring(1 + str.indexOf(sub)), sub, n - 1);
+        }
+    }
+
+    public static int strDist(String str, String sub) {
+        if (str.length() < sub.length()) {
+            return 0;
+        } else if (str.length() == sub.length()) {
+            return str.equals(sub) ? sub.length() : 0;
+        } else {
+            if (str.contains(sub)) {
+                if (str.substring(1).contains(sub)) {
+                    if (str.startsWith(sub)) {
+                        return sub.length() + str.substring(0, str.lastIndexOf(sub)).length();
+                    } else {
+                        return strDist(str.substring(1), sub);
+                    }
+                } else {
+                    return sub.length();
+                }
+            } else {
+                return 0;
+            }
+        }
+    }
 }
--- src/test/java/coding/bat/recursion/one/RecursionOneTestSuite.java
+++ src/test/java/coding/bat/recursion/one/RecursionOneTestSuite.java
@@ -81,4 +81,295 @@ public class RecursionOneTestSuite {
         Assert.assertEquals(13, RecursionOne.sumDigits(49));
         Assert.assertEquals(3, RecursionOne.sumDigits(12));
     }
+
+    @Test
+    public void testCount7() {
+        //Given
+
+        //When
+
+        //Then
+        Assert.assertEquals(2, RecursionOne.count7(717));
+        Assert.assertEquals(1, RecursionOne.count7(7));
+        Assert.assertEquals(0, RecursionOne.count7(123));
+    }
+
+    @Test
+    public void testCount8() {
+        //Given
+
+        //When
+
+        //Then
+        Assert.assertEquals(1, RecursionOne.count8(8));
+        Assert.assertEquals(2, RecursionOne.count8(818));
+        Assert.assertEquals(4, RecursionOne.count8(8818));
+    }
+
+    @Test
+    public void testPowerN() {
+        //Given
+
+        //When
+
+        //Then
+        Assert.assertEquals(3, RecursionOne.powerN(3, 1));
+        Assert.assertEquals(9, RecursionOne.powerN(3,2));
+        Assert.assertEquals(27, RecursionOne.powerN(3,3));
+    }
+
+    @Test
+    public void testCountX() {
+        //Given
+
+        //When
+
+        //Then
+        Assert.assertEquals(4, RecursionOne.countX("xxhixx"));
+        Assert.assertEquals(3, RecursionOne.countX("xhixhix"));
+        Assert.assertEquals(0, RecursionOne.countX("hi"));
+    }
+
+    @Test
+    public void testCountHi() {
+        //Given
+
+        //When
+
+        //Then
+        Assert.assertEquals(1, RecursionOne.countHi("xxhixx"));
+        Assert.assertEquals(2, RecursionOne.countHi("xhixhix"));
+        Assert.assertEquals(1, RecursionOne.countHi("hi"));
+    }
+
+    @Test
+    public void testChangeXY() {
+        //Given
+
+        //When
+
+        //Then
+        Assert.assertEquals("codey", RecursionOne.changeXY("codex"));
+        Assert.assertEquals("yyhiyy", RecursionOne.changeXY("xxhixx"));
+        Assert.assertEquals("yhiyhiy", RecursionOne.changeXY("xhixhix"));
+    }
+
+    @Test
+    public void testChangePi() {
+        //Given
+
+        //When
+
+        //Then
+        Assert.assertEquals("x3.14x", RecursionOne.changePi("xpix"));
+        Assert.assertEquals("3.143.14", RecursionOne.changePi("pipi"));
+        Assert.assertEquals("3.14p", RecursionOne.changePi("pip"));
+    }
+
+    @Test
+    public void testNoX() {
+        //Given
+
+        //When
+
+        //Then
+        Assert.assertEquals("ab", RecursionOne.noX("xaxb"));
+        Assert.assertEquals("abc", RecursionOne.noX("abc"));
+        Assert.assertEquals("", RecursionOne.noX("xx"));
+    }
+
+    @Test
+    public void testArray6() {
+        //Given
+
+        //When
+
+        //Then
+        Assert.assertTrue(RecursionOne.array6(new int[]{1, 6, 4}, 0));
+        Assert.assertFalse(RecursionOne.array6(new int[]{1, 4}, 0));
+        Assert.assertTrue(RecursionOne.array6(new int[]{6},0));
+        Assert.assertFalse(RecursionOne.array6(new int[]{}, 0));
+    }
+
+    @Test
+    public void testArray11() {
+        //Given
+
+        //When
+
+        //Then
+        Assert.assertEquals(1, RecursionOne.array11(new int[]{1, 2, 11}, 0));
+        Assert.assertEquals(2, RecursionOne.array11(new int[]{11, 11}, 0));
+        Assert.assertEquals(0, RecursionOne.array11(new int[]{1, 2, 3, 4}, 0));
+    }
+
+    @Test
+    public void testArray220() {
+        //Given
+
+        //When
+
+        //Then
+        Assert.assertTrue(RecursionOne.array220(new int[]{1, 2, 20}, 0));
+        Assert.assertTrue(RecursionOne.array220(new int[]{3, 30}, 0));
+        Assert.assertFalse(RecursionOne.array220(new int[]{3},0));
+        Assert.assertFalse(RecursionOne.array220(new int[]{2, 19, 4}, 0));
+    }
+
+    @Test
+    public void testAllStar() {
+        //Given
+
+        //When
+
+        //Then
+        Assert.assertEquals("h*e*l*l*o", RecursionOne.allStar("hello"));
+        Assert.assertEquals("a*b*c", RecursionOne.allStar("abc"));
+        Assert.assertEquals("a*b", RecursionOne.allStar("ab"));
+    }
+
+    @Test
+    public void testPairSet() {
+        //Given
+
+        //When
+
+        //Then
+        Assert.assertEquals("hel*lo", RecursionOne.pairStar("hello"));
+        Assert.assertEquals("x*xy*y", RecursionOne.pairStar("xxyy"));
+        Assert.assertEquals("a*a*a*a", RecursionOne.pairStar("aaaa"));
+    }
+
+    @Test
+    public void testEndX() {
+        //Given
+
+        //When
+
+        //Then
+        Assert.assertEquals("rexx", RecursionOne.endX("xxre"));
+        Assert.assertEquals("hixxxx", RecursionOne.endX("xxhixx"));
+        Assert.assertEquals("hihixxx", RecursionOne.endX("xhixhix"));
+    }
+
+    @Test
+    public void testCountPairs() {
+        //Given
+
+        //When
+
+        //Then
+        Assert.assertEquals(1, RecursionOne.countPairs("axa"));
+        Assert.assertEquals(2, RecursionOne.countPairs("axax"));
+        Assert.assertEquals(1, RecursionOne.countPairs("axbx"));
+    }
+
+    @Test
+    public void testCountAbc() {
+        //Given
+
+        //When
+
+        //Then
+        Assert.assertEquals(1, RecursionOne.countAbc("abc"));
+        Assert.assertEquals(2, RecursionOne.countAbc("abcxxabc"));
+        Assert.assertEquals(2, RecursionOne.countAbc("abaxxaba"));
+    }
+
+    @Test
+    public void testCount11() {
+        //Given
+
+        //When
+
+        //Then
+        Assert.assertEquals(2, RecursionOne.count11("11abc11"));
+        Assert.assertEquals(3, RecursionOne.count11("abc11x11x11"));
+        Assert.assertEquals(1, RecursionOne.count11("111"));
+    }
+
+    @Test
+    public void testStringClean() {
+        //Given
+
+        //When
+
+        //Then
+        Assert.assertEquals("yza", RecursionOne.stringClean("yyzzza"));
+        Assert.assertEquals("abcd", RecursionOne.stringClean("abbbcdd"));
+        Assert.assertEquals("Helo", RecursionOne.stringClean("Hello"));
+    }
+
+    @Test
+    public void testCountHi2() {
+        //Given
+
+        //When
+
+        //Then
+        Assert.assertEquals(1, RecursionOne.countHi2("ahixhi"));
+        Assert.assertEquals(2, RecursionOne.countHi2("ahibhi"));
+        Assert.assertEquals(0, RecursionOne.countHi2("xhixhi"));
+    }
+
+    @Test
+    public void testParenBit() {
+        //Given
+
+        //When
+
+        //Then
+        Assert.assertEquals("(abc)", RecursionOne.parenBit("xyz(abc)123"));
+        Assert.assertEquals("(hello)", RecursionOne.parenBit("x(hello)"));
+        Assert.assertEquals("(xy)", RecursionOne.parenBit("(xy)1"));
+    }
+
+    @Test
+    public void testNestParen() {
+        //Given
+
+        //When
+
+        //Then
+        Assert.assertTrue(RecursionOne.nestParen("(())"));
+        Assert.assertTrue(RecursionOne.nestParen("((()))"));
+        Assert.assertFalse(RecursionOne.nestParen("(((x))"));
+    }
+
+    @Test
+    public void testStrCount() {
+        //Given
+
+        //When
+
+        //Then
+        Assert.assertEquals(2, RecursionOne.strCount("catcowcat", "cat"));
+        Assert.assertEquals(1, RecursionOne.strCount("catcowcat", "cow"));
+        Assert.assertEquals(0, RecursionOne.strCount("catcowcat", "dog"));
+    }
+
+    @Test
+    public void testStrCopies() {
+        //Given
+
+        //When
+
+        //Then
+        Assert.assertTrue(RecursionOne.strCopies("catcowcat", "cat", 2));
+        Assert.assertFalse(RecursionOne.strCopies("catcowcat", "cow", 2));
+        Assert.assertTrue(RecursionOne.strCopies("catcowcat", "cow", 1));
+    }
+
+    @Test
+    public void testStrDist() {
+        //Given
+
+        //When
+
+        //Then
+        Assert.assertEquals(9, RecursionOne.strDist("catcowcat", "cat"));
+        Assert.assertEquals(3, RecursionOne.strDist("catcowcat", "cow"));
+        Assert.assertEquals(9, RecursionOne.strDist("cccatcowcatxx", "cat"));
+        Assert.assertEquals(5, RecursionOne.strDist("hiHellohihihi", "hih"));
+    }
 }
