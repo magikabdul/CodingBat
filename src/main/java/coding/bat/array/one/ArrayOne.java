12e8dd46c18eac8f693ba98cacfbb6713647fb15
Krzysztof Cholewa <krzysztof.cholewa@gmail.com>
2018-01-10 23:40:33 +0100

    array one in progress

--- src/main/java/coding/bat/array/one/ArrayOne.java
+++ src/main/java/coding/bat/array/one/ArrayOne.java
@@ -65,4 +65,76 @@ public class ArrayOne {
     public static boolean has23(int[] nums) {
         return nums[0] == 2 || nums[1] == 2 || nums[0] == 3 || nums[1] == 3;
     }
+
+    public static boolean no23(int[] nums) {
+        return nums[0] != 2 && nums[1] != 2 && nums[0] != 3 && nums[1] != 3;
+    }
+
+    public static int[] makeLast(int[] nums) {
+        int[] a = new int[2 * nums.length];
+
+        for (int i = 0; i < nums.length * 2;i++) {
+            a[i] = 0;
+        }
+
+        a[2 * nums.length - 1] = nums[nums.length - 1];
+        return a;
+    }
+
+    public static boolean double23(int[] nums) {
+        return nums.length == 2 && (nums[0] == 2 || nums[0] == 3) && nums[0] == nums[1];
+    }
+
+    public static int[] fix23(int[] nums) {
+        int[] array = new int[3];
+
+        array[0] = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i - 1] == 2 && nums[i] == 3) {
+                array[i] = 0;
+            } else {
+                array[i] = nums[i];
+            }
+        }
+        return array;
+    }
+
+    public static int start1(int[] a, int[] b) {
+        int result = 0;
+
+        if (a.length > 0 && a[0] == 1) {
+            result++;
+        }
+        if (b.length > 0 && b[0] == 1) {
+                result++;
+        }
+
+        return result;
+    }
+
+    public static int[] biggerTwo(int[] a, int[] b) {
+        return Arrays.stream(a).sum() >= Arrays.stream(b).sum() ? a : b;
+    }
+
+    public static int[] makeMiddle(int[] nums) {
+        return new int[]{nums[nums.length / 2 - 1], nums[nums.length / 2]};
+    }
+
+    public static int[] plusTwo(int[] a, int[] b) {
+        return new int[]{a[0], a[1], b[0], b[1]};
+    }
+
+    public static int[] swapEnds(int[] nums) {
+        int[] ints = new int[nums.length];
+
+        if (nums.length > 1) {
+            ints[0] = nums[nums.length - 1];
+            System.arraycopy(nums, 1, ints, 1, nums.length - 1);
+            ints[nums.length - 1] = nums[0];
+        } else {
+            ints = nums;
+        }
+
+        return ints;
+    }
 }
--- src/test/java/coding/bat/array/one/ArrayOneTestSuite.java
+++ src/test/java/coding/bat/array/one/ArrayOneTestSuite.java
@@ -221,4 +221,176 @@ public class ArrayOneTestSuite {
         Assert.assertTrue(result2);
         Assert.assertFalse(result3);
     }
+
+    @Test
+    public void testNo23() {
+        //Given
+
+        //When
+        boolean result1 = ArrayOne.no23(new int[]{4, 5});
+        boolean result2 = ArrayOne.no23(new int[]{4, 2});
+        boolean result3 = ArrayOne.no23(new int[]{3, 5});
+
+        //Then
+        Assert.assertTrue(result1);
+        Assert.assertFalse(result2);
+        Assert.assertFalse(result3);
+    }
+
+    @Test
+    public void testMakeLast() {
+        //Given
+
+        //When
+        int[] result1 = ArrayOne.makeLast(new int[]{4, 5, 6});
+        int[] result2 = ArrayOne.makeLast(new int[]{1, 2});
+        int[] result3 = ArrayOne.makeLast(new int[]{3});
+
+        //Then
+        Assert.assertEquals(6, result1.length);
+        Assert.assertEquals(6, result1[result1.length - 1]);
+        Assert.assertEquals(4, result2.length);
+        Assert.assertEquals(2, result2[result2.length - 1]);
+        Assert.assertEquals(2, result3.length);
+        Assert.assertEquals(3, result3[result3.length - 1]);
+    }
+
+    @Test
+    public void testDouble23() {
+        //Given
+
+        //When
+        boolean result1 = ArrayOne.double23(new int[]{2, 2});
+        boolean result2 = ArrayOne.double23(new int[]{3, 3});
+        boolean result3 = ArrayOne.double23(new int[]{2, 3});
+
+        //Then
+        Assert.assertTrue(result1);
+        Assert.assertTrue(result2);
+        Assert.assertFalse(result3);
+    }
+
+    @Test
+    public void testFix23() {
+        //Given
+
+        //When
+        int[] result1 = ArrayOne.fix23(new int[]{1, 2, 3});
+        int[] result2 = ArrayOne.fix23(new int[]{2, 3, 5});
+        int[] result3 = ArrayOne.fix23(new int[]{1, 2, 1});
+
+        //Then
+        Assert.assertEquals(1, result1[0]);
+        Assert.assertEquals(2, result1[1]);
+        Assert.assertEquals(0, result1[2]);
+        Assert.assertEquals(2, result2[0]);
+        Assert.assertEquals(0, result2[1]);
+        Assert.assertEquals(5, result2[2]);
+        Assert.assertEquals(1, result3[0]);
+        Assert.assertEquals(2, result3[1]);
+        Assert.assertEquals(1, result3[2]);
+    }
+
+    @Test
+    public void testStart1() {
+        //Given
+
+        //When
+        int result1 = ArrayOne.start1(new int[]{1, 2, 3}, new int[]{1, 3});
+        int result2 = ArrayOne.start1(new int[]{2, 3, 5}, new int[]{1});
+        int result3 = ArrayOne.start1(new int[]{1, 2, 1}, new int[]{});
+
+        //Then
+        Assert.assertEquals(2, result1);
+        Assert.assertEquals(1, result2);
+        Assert.assertEquals(1, result3);
+    }
+
+    @Test
+    public void testBiggerTwo() {
+        //Given
+
+        //When
+        int[] result1 = ArrayOne.biggerTwo(new int[]{1, 2}, new int[]{3, 4});
+        int[] result2 = ArrayOne.biggerTwo(new int[]{3, 4}, new int[]{3, 4});
+        int[] result3 = ArrayOne.biggerTwo(new int[]{1, 1}, new int[]{1, 2});
+
+        //Then
+        Assert.assertEquals(3, result1[0]);
+        Assert.assertEquals(4, result1[1]);
+        Assert.assertEquals(3, result2[0]);
+        Assert.assertEquals(4, result2[1]);
+        Assert.assertEquals(1, result3[0]);
+        Assert.assertEquals(2, result3[1]);
+    }
+
+    @Test
+    public void testMakeMiddle() {
+        //Given
+
+        //When
+        int[] result1 = ArrayOne.makeMiddle(new int[]{1, 2, 3, 4});
+        int[] result2 = ArrayOne.makeMiddle(new int[]{7, 1, 2, 3, 4, 9});
+        int[] result3 = ArrayOne.makeMiddle(new int[]{1, 2});
+
+        //Then
+        Assert.assertEquals(2, result1[0]);
+        Assert.assertEquals(3, result1[1]);
+        Assert.assertEquals(2, result2[0]);
+        Assert.assertEquals(3, result2[1]);
+        Assert.assertEquals(1, result3[0]);
+        Assert.assertEquals(2, result3[1]);
+    }
+
+    @Test
+    public void testPlusTwo() {
+        //Given
+
+        //When
+        int[] result1 = ArrayOne.plusTwo(new int[]{1, 2}, new int[]{3, 4});
+        int[] result2 = ArrayOne.plusTwo(new int[]{4, 4}, new int[]{2, 2});
+        int[] result3 = ArrayOne.plusTwo(new int[]{9, 2}, new int[]{3, 4});
+
+        //Then
+        Assert.assertEquals(1, result1[0]);
+        Assert.assertEquals(2, result1[1]);
+        Assert.assertEquals(3, result1[2]);
+        Assert.assertEquals(4, result1[3]);
+
+        Assert.assertEquals(4, result2[0]);
+        Assert.assertEquals(4, result2[1]);
+        Assert.assertEquals(2, result2[2]);
+        Assert.assertEquals(2, result2[3]);
+
+        Assert.assertEquals(9, result3[0]);
+        Assert.assertEquals(2, result3[1]);
+        Assert.assertEquals(3, result3[2]);
+        Assert.assertEquals(4, result3[3]);
+    }
+
+    @Test
+    public void testSwapEnds() {
+        //Given
+
+        //When
+        int[] result1 = ArrayOne.swapEnds(new int[]{1, 2, 3, 4});
+        int[] result2 = ArrayOne.swapEnds(new int[]{1, 2, 3});
+        int[] result3 = ArrayOne.swapEnds(new int[]{8, 6, 7, 9, 5});
+
+        //Then
+        Assert.assertEquals(4, result1[0]);
+        Assert.assertEquals(2, result1[1]);
+        Assert.assertEquals(3, result1[2]);
+        Assert.assertEquals(1, result1[3]);
+
+        Assert.assertEquals(3, result2[0]);
+        Assert.assertEquals(2, result2[1]);
+        Assert.assertEquals(1, result2[2]);
+
+        Assert.assertEquals(5, result3[0]);
+        Assert.assertEquals(6, result3[1]);
+        Assert.assertEquals(7, result3[2]);
+        Assert.assertEquals(9, result3[3]);
+        Assert.assertEquals(8, result3[4]);
+    }
 }
