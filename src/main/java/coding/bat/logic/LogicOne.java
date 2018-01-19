1520c08ad9a32c3ed28b3302b53dfdad7596fc6f
Krzysztof Cholewa <krzysztof.cholewa@gmail.com>
2018-01-19 23:34:58 +0100

    logic one done

--- src/main/java/coding/bat/logic/LogicOne.java
+++ src/main/java/coding/bat/logic/LogicOne.java
@@ -68,4 +68,138 @@ public class LogicOne {
     public static boolean less20(int n) {
         return n % 20 == 18 || n % 20 == 19;
     }
+
+    public static boolean nearTen(int num) {
+        int expression = (num - (10 * (num / 10))) % 10;
+        return expression <= 2 || expression >= 8;
+    }
+
+    public static int teenSum(int a, int b) {
+        return (a >= 13 && a <= 19 || b >= 13 && b <= 19) ? 19 : a + b;
+    }
+
+    public static boolean answerCell(boolean isMorning, boolean isMom, boolean isAsleep) {
+        return (!isMorning || isMom) && !isAsleep;
+    }
+
+    public static int teaParty(int tea, int candy) {
+        if (tea >= 5 && candy >= 5) {
+            return tea / candy >= 2 || candy / tea >= 2 ? 2 : 1;
+        } else {
+            return 0;
+        }
+    }
+
+    public static String fizzString(String str) {
+        StringBuilder sb = new StringBuilder();
+        if (str.substring(0,1).equals("f")) {
+            sb.append("Fizz");
+        }
+        if (str.substring(str.length() - 1).equals("b")) {
+            sb.append("Buzz");
+        }
+
+        return sb.length() > 0 ? sb.toString() : str;
+    }
+
+    public static String fizzString2(int n) {
+        StringBuilder sb = new StringBuilder();
+
+        if (n % 3 == 0) {
+            sb.append("Fizz");
+        }
+        if (n % 5 == 0) {
+            sb.append("Buzz");
+        }
+
+        return sb.length() == 0 ? (n + "!") : sb.append("!").toString();
+    }
+
+    public static boolean twoAsOne(int a, int b, int c) {
+        return a + b == c || a + c == b || b + c == a;
+    }
+
+    public static boolean inOrder(int a, int b, int c, boolean bOk) {
+        return (b > a && c > b) || (bOk && c > b);
+    }
+
+    public static boolean inOrderEqual(int a, int b, int c, boolean equalOk) {
+        return (b > a && c > b) || (equalOk && b >= a && c >= b);
+    }
+
+    public static boolean lastDigit(int a, int b, int c) {
+        return a % 10 == b % 10 || a % 10 == c % 10 || b % 10 == c % 10;
+    }
+
+    public static boolean lessBy10(int a, int b, int c) {
+        return a - b >= 10 || a - c >= 10 || b - c >= 10 || b - a >= 10 || c - a >= 10 || c - b >= 10;
+    }
+
+    public static int withoutDoubles(int die1, int die2, boolean noDoubles) {
+        if (!noDoubles || die1 != die2) {
+            return die1 + die2;
+        } else {
+            if (die1 == 6) {
+                return 1 + die2;
+            } else {
+                return die1 + die2 + 1;
+            }
+        }
+    }
+
+    public static int maxMod5(int a, int b) {
+        if (a == b) {
+            return 0;
+        } else {
+            if (a % 5 == b % 5) {
+                return a > b ? b : a;
+            } else {
+                return a > b ? a : b;
+            }
+        }
+    }
+
+    public static int redTicket(int a, int b, int c) {
+        if (a == 2 && b == 2 && c == 2) {
+            return 10;
+        } else if (a == b && b == c) {
+            return 5;
+        } else if (b != a && c != a) {
+            return 1;
+        } else {
+            return 0;
+        }
+    }
+
+    public static int greenTicket(int a, int b, int c) {
+        if (a == b && b == c) {
+            return 20;
+        } else if (a == b || a == c || b == c) {
+            return 10;
+        } else {
+            return 0;
+        }
+    }
+
+    public static int blueTicket(int a, int b, int c) {
+        if (a + b == 10 || a + c == 10 || b + c == 10) {
+            return 10;
+        } else if (a + b == b + c + 10 || a + b == a + c + 10) {
+            return 5;
+        } else {
+            return 0;
+        }
+    }
+
+    public static boolean shareDigit(int a, int b) {
+        return a / 10 == b / 10 || a / 10 == b % 10 || a % 10 == b % 10 || a % 10 == b / 10;
+    }
+
+    public static int sumLimit(int a, int b) {
+        if (String.valueOf(a + b).length() != String.valueOf(a).length()) {
+            return a;
+        } else {
+            return a + b;
+        }
+    }
 }
--- src/test/java/coding/bat/logic/one/LogicOneTestSuite.java
+++ src/test/java/coding/bat/logic/one/LogicOneTestSuite.java
@@ -185,4 +185,274 @@ public class LogicOneTestSuite {
         Assert.assertTrue(result2);
         Assert.assertFalse(result3);
     }
+
+    @Test
+    public void testNearTen() {
+        //Given
+
+        //When
+        boolean result1 = LogicOne.nearTen(12);
+        boolean result2 = LogicOne.nearTen(17);
+        boolean result3 = LogicOne.nearTen(39);
+
+        //Then
+        Assert.assertTrue(result1);
+        Assert.assertFalse(result2);
+        Assert.assertTrue(result3);
+    }
+
+    @Test
+    public void testTeenSum() {
+        //Given
+
+        //When
+        int result1 = LogicOne.teenSum(3, 4);
+        int result2 = LogicOne.teenSum(10, 13);
+        int result3 = LogicOne.teenSum(13, 2);
+
+        //Then
+        Assert.assertEquals(7, result1);
+        Assert.assertEquals(19, result2);
+        Assert.assertEquals(19, result3);
+    }
+
+    @Test
+    public void testAnswerCell() {
+        //Given
+
+        //When
+        boolean result1 = LogicOne.answerCell(false, false, false);
+        boolean result2 = LogicOne.answerCell(false,false,true);
+        boolean result3 = LogicOne.answerCell(true,false,false);
+
+        //Then
+        Assert.assertTrue(result1);
+        Assert.assertFalse(result2);
+        Assert.assertFalse(result3);
+    }
+
+    @Test
+    public void testTeaParty() {
+        //Given
+
+        //When
+        int result1 = LogicOne.teaParty(6, 8);
+        int result2 = LogicOne.teaParty(3, 8);
+        int result3 = LogicOne.teaParty(20, 6);
+
+        //Then
+        Assert.assertEquals(1, result1);
+        Assert.assertEquals(0, result2);
+        Assert.assertEquals(2, result3);
+    }
+
+    @Test
+    public void testFizzString() {
+        //Given
+
+        //When
+        String result1 = LogicOne.fizzString("fig");
+        String result2 = LogicOne.fizzString("dib");
+        String result3 = LogicOne.fizzString("fib");
+
+        //Then
+        Assert.assertEquals("Fizz", result1);
+        Assert.assertEquals("Buzz", result2);
+        Assert.assertEquals("FizzBuzz", result3);
+    }
+
+    @Test
+    public void testFizzString2() {
+        //Given
+
+        //When
+        String result1 = LogicOne.fizzString2(1);
+        String result2 = LogicOne.fizzString2(2);
+        String result3 = LogicOne.fizzString2(3);
+
+        //Then
+        Assert.assertEquals("1!", result1);
+        Assert.assertEquals("2!", result2);
+        Assert.assertEquals("Fizz!", result3);
+    }
+
+    @Test
+    public void testTwoAsOne() {
+        //Given
+
+        //When
+        boolean result1 = LogicOne.twoAsOne(1,2,3);
+        boolean result2 = LogicOne.twoAsOne(3,1,2);
+        boolean result3 = LogicOne.twoAsOne(3,2,2);
+
+        //Then
+        Assert.assertTrue(result1);
+        Assert.assertTrue(result2);
+        Assert.assertFalse(result3);
+    }
+
+    @Test
+    public void testInOrder() {
+        //Given
+
+        //When
+        boolean result1 = LogicOne.inOrder(1,2,4, false);
+        boolean result2 = LogicOne.inOrder(1,2,1, false);
+        boolean result3 = LogicOne.inOrder(1,1,2, true);
+
+        //Then
+        Assert.assertTrue(result1);
+        Assert.assertFalse(result2);
+        Assert.assertTrue(result3);
+    }
+
+    @Test
+    public void testInOrderEqual() {
+        //Given
+
+        //When
+        boolean result1 = LogicOne.inOrderEqual(2,5,11, false);
+        boolean result2 = LogicOne.inOrderEqual(5,7,6, false);
+        boolean result3 = LogicOne.inOrderEqual(5,5,7, true);
+
+        //Then
+        Assert.assertTrue(result1);
+        Assert.assertFalse(result2);
+        Assert.assertTrue(result3);
+    }
+
+    @Test
+    public void testLastDigit() {
+        //Given
+
+        //When
+        boolean result1 = LogicOne.lastDigit(23,19,13);
+        boolean result2 = LogicOne.lastDigit(23,19,12);
+        boolean result3 = LogicOne.lastDigit(23,19,3);
+
+        //Then
+        Assert.assertTrue(result1);
+        Assert.assertFalse(result2);
+        Assert.assertTrue(result3);
+    }
+
+    @Test
+    public void testLessBy10() {
+        //Given
+
+        //When
+        boolean result1 = LogicOne.lessBy10(1,7,11);
+        boolean result2 = LogicOne.lessBy10(1,7,10);
+        boolean result3 = LogicOne.lessBy10(11,1,7);
+
+        //Then
+        Assert.assertTrue(result1);
+        Assert.assertFalse(result2);
+        Assert.assertTrue(result3);
+    }
+
+    @Test
+    public void testWithoutDoubles() {
+        //Given
+
+        //When
+        int result1 = LogicOne.withoutDoubles(2, 3,true);
+        int result2 = LogicOne.withoutDoubles(3, 3,true);
+        int result3 = LogicOne.withoutDoubles(3, 3,false);
+
+        //Then
+        Assert.assertEquals(5, result1);
+        Assert.assertEquals(7, result2);
+        Assert.assertEquals(6, result3);
+    }
+
+    @Test
+    public void testMaxMod5() {
+        //Given
+
+        //When
+        int result1 = LogicOne.maxMod5(2, 3);
+        int result2 = LogicOne.maxMod5(6, 2);
+        int result3 = LogicOne.maxMod5(3, 2);
+
+        //Then
+        Assert.assertEquals(3, result1);
+        Assert.assertEquals(6, result2);
+        Assert.assertEquals(3, result3);
+    }
+
+    @Test
+    public void testRedTicket() {
+        //Given
+
+        //When
+        int result1 = LogicOne.redTicket(2, 2,2);
+        int result2 = LogicOne.redTicket(2, 2,1);
+        int result3 = LogicOne.redTicket(0, 0,0);
+
+        //Then
+        Assert.assertEquals(10, result1);
+        Assert.assertEquals(0, result2);
+        Assert.assertEquals(5, result3);
+    }
+
+    @Test
+    public void testGreenTicket() {
+        //Given
+
+        //When
+        int result1 = LogicOne.greenTicket(1, 2,3);
+        int result2 = LogicOne.greenTicket(2, 2,2);
+        int result3 = LogicOne.greenTicket(1, 1,2);
+
+        //Then
+        Assert.assertEquals(0, result1);
+        Assert.assertEquals(20, result2);
+        Assert.assertEquals(10, result3);
+    }
+
+    @Test
+    public void testBlueTicket() {
+        //Given
+
+        //When
+        int result1 = LogicOne.blueTicket(9, 1,0);
+        int result2 = LogicOne.blueTicket(9, 2,0);
+        int result3 = LogicOne.blueTicket(6, 1,4);
+
+        //Then
+        Assert.assertEquals(10, result1);
+        Assert.assertEquals(0, result2);
+        Assert.assertEquals(10, result3);
+    }
+
+    @Test
+    public void testShareDigit() {
+        //Given
+
+        //When
+        boolean result1 = LogicOne.shareDigit(12,23);
+        boolean result2 = LogicOne.shareDigit(12,43);
+        boolean result3 = LogicOne.shareDigit(12,44);
+
+        //Then
+        Assert.assertTrue(result1);
+        Assert.assertFalse(result2);
+        Assert.assertFalse(result3);
+    }
+
+    @Test
+    public void testSumLimit() {
+        //Given
+
+        //When
+        int result1 = LogicOne.sumLimit(2,3);
+        int result2 = LogicOne.sumLimit(8,3);
+        int result3 = LogicOne.sumLimit(8,1);
+
+        //Then
+        Assert.assertEquals(5, result1);
+        Assert.assertEquals(8, result2);
+        Assert.assertEquals(9, result3);
+    }
 }
